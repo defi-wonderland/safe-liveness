0e6eb406a72fd2cf7c4678ab5c42c51f356a6b90
OneTony <onetony@defi.sucks>
2023-11-16 17:37:29 +0200

    fix: linting warnings

--- solidity/interfaces/ISafe.sol
+++ solidity/interfaces/ISafe.sol
@@ -28,31 +28,31 @@ interface ISafe {
    *      - if the contract at `to` address has code or not
    *      - if the `gasToken` is a contract or not
    *      It is the responsibility of the caller to perform such checks.
-   * @param to Destination address of Safe transaction.
-   * @param value Ether value of Safe transaction.
-   * @param data Data payload of Safe transaction.
-   * @param operation Operation type of Safe transaction.
-   * @param safeTxGas Gas that should be used for the Safe transaction.
-   * @param baseGas Gas costs that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
-   * @param gasPrice Gas price that should be used for the payment calculation.
-   * @param gasToken Token address (or 0 if ETH) that is used for the payment.
-   * @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
-   * @param signatures Signature data that should be verified.
+   * @param _to Destination address of Safe transaction.
+   * @param _value Ether value of Safe tsransaction.
+   * @param _data Data payload of Safe transaction.
+   * @param _operation Operation type of Safe transaction.
+   * @param _safeTxGas Gas that should be used for the Safe transaction.
+   * @param _baseGas Gas costs that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+   * @param _gasPrice Gas price that should be used for the payment calculation.
+   * @param _gasToken Token address (or 0 if ETH) that is used for the payment.
+   * @param _refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+   * @param _signatures Signature data that should be verified.
    *                   Can be packed ECDSA signature ({bytes32 r}{bytes32 s}{uint8 v}), contract signature (EIP-1271) or approved hash.
-   * @return success Boolean indicating transaction's success.
+   * @return _success Boolean indicating transaction's success.
    */
   function execTransaction(
-    address to,
-    uint256 value,
-    bytes calldata data,
-    Enum.Operation operation,
-    uint256 safeTxGas,
-    uint256 baseGas,
-    uint256 gasPrice,
-    address gasToken,
-    address payable refundReceiver,
-    bytes memory signatures
-  ) external payable returns (bool success);
+    address _to,
+    uint256 _value,
+    bytes calldata _data,
+    Enum.Operation _operation,
+    uint256 _safeTxGas,
+    uint256 _baseGas,
+    uint256 _gasPrice,
+    address _gasToken,
+    address payable _refundReceiver,
+    bytes memory _signatures
+  ) external payable returns (bool _success);
 
   /**
    * @dev Set a guard that checks transactions before execution
@@ -61,22 +61,22 @@ interface ISafe {
    *        a broken guard can cause a denial of service for the Safe. Make sure to carefully
    *        audit the guard code and design recovery mechanisms.
    * @notice Set Transaction Guard `guard` for the Safe. Make sure you trust the guard.
-   * @param guard The address of the guard to be used or the 0 address to disable the guard
+   * @param _guard The address of the guard to be used or the 0 address to disable the guard
    */
-  function setGuard(address guard) external;
+  function setGuard(address _guard) external;
 
   /**
    * @notice Enables the module `module` for the Safe.
    * @dev This can only be done via a Safe transaction.
-   * @param module Module to be whitelisted.
+   * @param _module Module to be whitelisted.
    */
-  function enableModule(address module) external;
+  function enableModule(address _module) external;
 
   /**
    * @notice Returns the number of required confirmations for a Safe transaction aka the threshold.
-   * @return Threshold number.
+   * @return _threshold Threshold number.
    */
-  function getThreshold() external view returns (uint256);
+  function getThreshold() external view returns (uint256 _threshold);
 
   /**
    * @notice Changes the threshold of the Safe to `_threshold`.
@@ -87,22 +87,23 @@ interface ISafe {
 
   /**
    * @notice Returns a list of Safe owners.
-   * @return Array of Safe owners.
+   * @return _owners Array of Safe owners.
    */
-  function getOwners() external view returns (address[] memory);
+  function getOwners() external view returns (address[] memory _owners);
 
   /**
    * @notice Adds the owner `owner` to the Safe and updates the threshold to `_threshold`.
    * @dev This can only be done via a Safe transaction.
-   * @param owner New owner address.
+   * @param _owner New owner address.
    * @param _threshold New threshold.
    */
-  function addOwnerWithThreshold(address owner, uint256 _threshold) external;
+  function addOwnerWithThreshold(address _owner, uint256 _threshold) external;
 
   /**
    * @notice Returns the nonce of the safe
+   * @return _nonce Current nonce.
    */
-  function nonce() external view returns (uint256);
+  function nonce() external view returns (uint256 _nonce);
 
   /**
    * @notice Sets an initial storage of the Safe contract.
@@ -110,48 +111,48 @@ interface ISafe {
    *      If a proxy was created without setting up, anyone can call setup and claim the proxy.
    * @param _owners List of Safe owners.
    * @param _threshold Number of required confirmations for a Safe transaction.
-   * @param to Contract address for optional delegate call.
-   * @param data Data payload for optional delegate call.
-   * @param fallbackHandler Handler for fallback calls to this contract
-   * @param paymentToken Token that should be used for the payment (0 is ETH)
-   * @param payment Value that should be paid
-   * @param paymentReceiver Address that should receive the payment (or 0 if tx.origin)
+   * @param _to Contract address for optional delegate call.
+   * @param _data Data payload for optional delegate call.
+   * @param _fallbackHandler Handler for fallback calls to this contract
+   * @param _paymentToken Token that should be used for the payment (0 is ETH)
+   * @param _payment Value that should be paid
+   * @param _paymentReceiver Address that should receive the payment (or 0 if tx.origin)
    */
   function setup(
     address[] calldata _owners,
     uint256 _threshold,
-    address to,
-    bytes calldata data,
-    address fallbackHandler,
-    address paymentToken,
-    uint256 payment,
-    address payable paymentReceiver
+    address _to,
+    bytes calldata _data,
+    address _fallbackHandler,
+    address _paymentToken,
+    uint256 _payment,
+    address payable _paymentReceiver
   ) external;
 
   /**
    * @notice Returns the pre-image of the transaction hash (see getTransactionHash).
-   * @param to Destination address.
-   * @param value Ether value.
-   * @param data Data payload.
-   * @param operation Operation type.
-   * @param safeTxGas Gas that should be used for the safe transaction.
-   * @param baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
-   * @param gasPrice Maximum gas price that should be used for this transaction.
-   * @param gasToken Token address (or 0 if ETH) that is used for the payment.
-   * @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+   * @param _to Destination address.
+   * @param _value Ether value.
+   * @param _data Data payload.
+   * @param _operation Operation type.
+   * @param _safeTxGas Gas that should be used for the safe transaction.
+   * @param _baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+   * @param _gasPrice Maximum gas price that should be used for this transaction.
+   * @param _gasToken Token address (or 0 if ETH) that is used for the payment.
+   * @param _refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
    * @param _nonce Transaction nonce.
-   * @return Transaction hash bytes.
+   * @return _tx Transaction hash bytes.
    */
   function encodeTransactionData(
-    address to,
-    uint256 value,
-    bytes calldata data,
-    Enum.Operation operation,
-    uint256 safeTxGas,
-    uint256 baseGas,
-    uint256 gasPrice,
-    address gasToken,
-    address refundReceiver,
+    address _to,
+    uint256 _value,
+    bytes calldata _data,
+    Enum.Operation _operation,
+    uint256 _safeTxGas,
+    uint256 _baseGas,
+    uint256 _gasPrice,
+    address _gasToken,
+    address _refundReceiver,
     uint256 _nonce
-  ) external view returns (bytes memory);
+  ) external view returns (bytes memory _tx);
 }
