9f7c9904fcf842bfb8d69cfad16e49171a8fd312
OneTony <onetony@defi.sucks>
2023-11-16 16:42:42 +0200

    feat: update e2e base

--- solidity/interfaces/ISafe.sol
+++ solidity/interfaces/ISafe.sol
@@ -18,7 +18,41 @@ interface ISafe {
     uint256 _value,
     bytes memory _data,
     Enum.Operation _operation
-  ) external virtual returns (bool _success);
+  ) external returns (bool _success);
+
+  /**
+   * @notice Executes a `operation` {0: Call, 1: DelegateCall}} transaction to `to` with `value` (Native Currency)
+   *          and pays `gasPrice` * `gasLimit` in `gasToken` token to `refundReceiver`.
+   * @dev The fees are always transferred, even if the user transaction fails.
+   *      This method doesn't perform any sanity check of the transaction, such as:
+   *      - if the contract at `to` address has code or not
+   *      - if the `gasToken` is a contract or not
+   *      It is the responsibility of the caller to perform such checks.
+   * @param to Destination address of Safe transaction.
+   * @param value Ether value of Safe transaction.
+   * @param data Data payload of Safe transaction.
+   * @param operation Operation type of Safe transaction.
+   * @param safeTxGas Gas that should be used for the Safe transaction.
+   * @param baseGas Gas costs that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+   * @param gasPrice Gas price that should be used for the payment calculation.
+   * @param gasToken Token address (or 0 if ETH) that is used for the payment.
+   * @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+   * @param signatures Signature data that should be verified.
+   *                   Can be packed ECDSA signature ({bytes32 r}{bytes32 s}{uint8 v}), contract signature (EIP-1271) or approved hash.
+   * @return success Boolean indicating transaction's success.
+   */
+  function execTransaction(
+    address to,
+    uint256 value,
+    bytes calldata data,
+    Enum.Operation operation,
+    uint256 safeTxGas,
+    uint256 baseGas,
+    uint256 gasPrice,
+    address gasToken,
+    address payable refundReceiver,
+    bytes memory signatures
+  ) external payable returns (bool success);
 
   /**
    * @dev Set a guard that checks transactions before execution
@@ -37,4 +71,87 @@ interface ISafe {
    * @param module Module to be whitelisted.
    */
   function enableModule(address module) external;
+
+  /**
+   * @notice Returns the number of required confirmations for a Safe transaction aka the threshold.
+   * @return Threshold number.
+   */
+  function getThreshold() external view returns (uint256);
+
+  /**
+   * @notice Changes the threshold of the Safe to `_threshold`.
+   * @dev This can only be done via a Safe transaction.
+   * @param _threshold New threshold.
+   */
+  function changeThreshold(uint256 _threshold) external;
+
+  /**
+   * @notice Returns a list of Safe owners.
+   * @return Array of Safe owners.
+   */
+  function getOwners() external view returns (address[] memory);
+
+  /**
+   * @notice Adds the owner `owner` to the Safe and updates the threshold to `_threshold`.
+   * @dev This can only be done via a Safe transaction.
+   * @param owner New owner address.
+   * @param _threshold New threshold.
+   */
+  function addOwnerWithThreshold(address owner, uint256 _threshold) external;
+
+  /**
+   * @notice Returns the nonce of the safe
+   */
+  function nonce() external view returns (uint256);
+
+  /**
+   * @notice Sets an initial storage of the Safe contract.
+   * @dev This method can only be called once.
+   *      If a proxy was created without setting up, anyone can call setup and claim the proxy.
+   * @param _owners List of Safe owners.
+   * @param _threshold Number of required confirmations for a Safe transaction.
+   * @param to Contract address for optional delegate call.
+   * @param data Data payload for optional delegate call.
+   * @param fallbackHandler Handler for fallback calls to this contract
+   * @param paymentToken Token that should be used for the payment (0 is ETH)
+   * @param payment Value that should be paid
+   * @param paymentReceiver Address that should receive the payment (or 0 if tx.origin)
+   */
+  function setup(
+    address[] calldata _owners,
+    uint256 _threshold,
+    address to,
+    bytes calldata data,
+    address fallbackHandler,
+    address paymentToken,
+    uint256 payment,
+    address payable paymentReceiver
+  ) external;
+
+  /**
+   * @notice Returns the pre-image of the transaction hash (see getTransactionHash).
+   * @param to Destination address.
+   * @param value Ether value.
+   * @param data Data payload.
+   * @param operation Operation type.
+   * @param safeTxGas Gas that should be used for the safe transaction.
+   * @param baseGas Gas costs for that are independent of the transaction execution(e.g. base transaction fee, signature check, payment of the refund)
+   * @param gasPrice Maximum gas price that should be used for this transaction.
+   * @param gasToken Token address (or 0 if ETH) that is used for the payment.
+   * @param refundReceiver Address of receiver of gas payment (or 0 if tx.origin).
+   * @param _nonce Transaction nonce.
+   * @return Transaction hash bytes.
+   */
+  function encodeTransactionData(
+    address to,
+    uint256 value,
+    bytes calldata data,
+    Enum.Operation operation,
+    uint256 safeTxGas,
+    uint256 baseGas,
+    uint256 gasPrice,
+    address gasToken,
+    address refundReceiver,
+    uint256 _nonce
+  ) external view returns (bytes memory);
 }
--- solidity/test/e2e/Common.sol
+++ solidity/test/e2e/Common.sol
@@ -4,38 +4,115 @@ pragma solidity =0.8.19;
 import {DSTestPlus} from '@defi-wonderland/solidity-utils/solidity/test/DSTestPlus.sol';
 import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 import {SafeProxy} from 'safe-contracts/proxies/SafeProxy.sol';
+import {Enum} from 'safe-contracts/common/Enum.sol';
 import {StorageMirror} from 'contracts/StorageMirror.sol';
 import {UpdateStorageMirrorGuard} from 'contracts/UpdateStorageMirrorGuard.sol';
+import {GuardCallbackModule} from 'contracts/GuardCallbackModule.sol';
 import {IGuardCallbackModule} from 'interfaces/IGuardCallbackModule.sol';
+import {ISafe} from 'interfaces/ISafe.sol';
 import {IGnosisSafeProxyFactory} from 'test/e2e/IGnosisSafeProxyFactory.sol';
-import {TestConstants} from 'test/TestConstants.sol';
+import {TestConstants} from 'test/utils/TestConstants.sol';
+import {ContractDeploymentAddress} from 'test/utils/ContractDeploymentAddress.sol';
 
 contract CommonE2EBase is DSTestPlus, TestConstants {
   uint256 internal constant _FORK_BLOCK = 15_452_788;
 
   address public deployer = makeAddr('deployer');
-  address public safeOwner = makeAddr('safeOwner');
   address public proposer = makeAddr('proposer');
+  address public safeOwner;
+  uint256 public safeOwnerKey;
 
   StorageMirror public storageMirror;
   UpdateStorageMirrorGuard public updateStorageMirrorGuard;
-  SafeProxy public safe;
-  IGuardCallbackModule public guardCallbackModule = IGuardCallbackModule(makeAddr('guardCallbackModule'));
+  GuardCallbackModule public guardCallbackModule;
+  ISafe public safe;
   IGnosisSafeProxyFactory public gnosisSafeProxyFactory = IGnosisSafeProxyFactory(GNOSIS_SAFE_PROXY_FACTORY);
 
   function setUp() public virtual {
     vm.createSelectFork(vm.rpcUrl('mainnet'), _FORK_BLOCK);
 
+    // Make address and key of safe owner
+    (safeOwner, safeOwnerKey) = makeAddrAndKey('safeOwner');
+
     vm.prank(safeOwner);
-    safe = gnosisSafeProxyFactory.createProxy(GNOSIS_SAFE_SINGLETON, '');
+    safe = ISafe(address(gnosisSafeProxyFactory.createProxy(GNOSIS_SAFE_SINGLETON, ''))); // safeOwner nonce 0
     label(address(safe), 'SafeProxy');
 
+    address _updateStorageMirrorGuardTheoriticalAddress = ContractDeploymentAddress.addressFrom(deployer, 2);
+
     vm.prank(deployer);
-    storageMirror = new StorageMirror();
+    storageMirror = new StorageMirror(); // deployer nonce 0
     label(address(storageMirror), 'StorageMirror');
 
     vm.prank(deployer);
-    updateStorageMirrorGuard = new UpdateStorageMirrorGuard(guardCallbackModule);
+    guardCallbackModule = new GuardCallbackModule(address(storageMirror), _updateStorageMirrorGuardTheoriticalAddress); // deployer nonce 1
+    label(address(guardCallbackModule), 'GuardCallbackModule');
+
+    vm.prank(deployer);
+    updateStorageMirrorGuard = new UpdateStorageMirrorGuard(guardCallbackModule); // deployer nonce 2
     label(address(updateStorageMirrorGuard), 'UpdateStorageMirrorGuard');
+
+    // Make sure the theoritical address was calculated correctly
+    assert(address(updateStorageMirrorGuard) == _updateStorageMirrorGuardTheoriticalAddress);
+
+    // Set up safe
+    address[] memory _owners = new address[](1);
+    _owners[0] = safeOwner;
+    vm.prank(safeOwner); // safeOwner nonce 1
+    safe.setup(_owners, 1, address(safe), bytes(''), address(0), address(0), 0, payable(0));
+
+    // Enable guard callback module
+    enableModule(safe, safeOwner, safeOwnerKey, address(guardCallbackModule));
+
+    // data to sign and send to set the guard
+    bytes memory _setGuardData = abi.encodeWithSelector(IGuardCallbackModule.setGuard.selector);
+    bytes memory _setGuardEncodedTxData = safe.encodeTransactionData(
+      address(guardCallbackModule), 0, _setGuardData, Enum.Operation.Call, 0, 0, 0, address(0), payable(0), safe.nonce()
+    );
+
+    // signature
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(safeOwnerKey, keccak256(_setGuardEncodedTxData));
+    bytes memory _setGuardSignature = abi.encodePacked(_r, _s, _v);
+
+    // execute setup of guard
+    vm.prank(safeOwner);
+    safe.execTransaction(
+      address(guardCallbackModule),
+      0,
+      _setGuardData,
+      Enum.Operation.Call,
+      0,
+      0,
+      0,
+      address(0),
+      payable(0),
+      _setGuardSignature
+    );
+  }
+
+  /**
+   * @notice Enables a module for the given safe
+   * @param _safe The safe that will enable the module
+   * @param _safeOwner The address of the owner of the safe
+   * @param _safeOwnerKey The private key to sign the tx
+   * @param _module The module address to enable
+   */
+  function enableModule(ISafe _safe, address _safeOwner, uint256 _safeOwnerKey, address _module) public {
+    uint256 _safeNonce = _safe.nonce();
+    // data to sign to enable module
+    bytes memory _enableModuleData = abi.encodeWithSelector(ISafe.enableModule.selector, address(_module));
+    bytes memory _enableModuleEncodedTxData = _safe.encodeTransactionData(
+      address(_safe), 0, _enableModuleData, Enum.Operation.Call, 0, 0, 0, address(0), payable(0), _safeNonce
+    );
+
+    // signature
+    (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(_safeOwnerKey, keccak256(_enableModuleEncodedTxData));
+    bytes memory _enableModuleSignature = abi.encodePacked(_r, _s, _v);
+
+    // execute enable module
+    vm.prank(_safeOwner);
+    _safe.execTransaction(
+      address(_safe), 0, _enableModuleData, Enum.Operation.Call, 0, 0, 0, address(0), payable(0), _enableModuleSignature
+    );
   }
 }
