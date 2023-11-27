c29b80a392cf06695211f39a8fd746694148d794
excaliborr <excalibor@defi.sucks>
2023-11-27 10:14:39 -0500

    chore: remove deployments and unused imports

--- solidity/contracts/BlockHeaderOracle.sol
+++ solidity/contracts/BlockHeaderOracle.sol
@@ -2,7 +2,6 @@
 pragma solidity =0.8.19;
 
 import {IBlockHeaderOracle} from 'interfaces/IBlockHeaderOracle.sol';
-import {StateVerifier} from 'libraries/StateVerifier.sol';
 
 /**
  * @title BlockHeaderOracle
